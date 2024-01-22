1aa412f4d1545509c74b3f3a1958f7023887b7be
qiuzhu <1275286075@qq.com>
2024-01-22 16:19:05 +0800

    springboot集成flowable  V2功能实现：集成flowable-ui，能够通过BPMN在线进行流程设计及发布等

--- flowabletest/src/main/resources/static/js/config.js
+++
@@ -1,7 +0,0 @@
-//获取contextPath
-function getContextPath() {
-    var pathName = document.location.pathname;
-    var index = pathName.substr(1).indexOf("/");
-    var result = pathName.substr(0,index+1);
-    return result;
-}
